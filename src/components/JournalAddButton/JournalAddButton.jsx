36cab7a8b29690cc227b6a0048cce543d6cb92d7
zhurakovska <n.sinawka@gmail.com>
2023-10-13 14:54:39 +0300

    add

---
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import CardButton from "../CardButton/CardButton";
+import "./JournalAddButton.css";
+
+export const JournalAddButton = () => {
+  return <CardButton>Новое воспомінаніе</CardButton>;
+};
---
+++ src/components/JournalList/JournalList.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./JournalList.css";
+
+export const JournalList = ({ children }) => {
+  return <div className="journal-list">{children}</div>;
+};
